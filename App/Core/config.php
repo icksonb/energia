9e5e5625dca7da4cd7ab4ff874b0441ae9e06b76
icksonb <ickson94@gmail.com>
2018-04-25 11:35:21 -0300

    Mudança de IP

--- App/Core/config.php
+++ App/Core/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define("BASE_SITE_HOST", "http://192.168.0.17");
+define("BASE_SITE_HOST", "http://localhost");
 define("BASE_SITE_MENU", BASE_SITE_HOST."/projeto/");
 define("BASE_ASSETS", BASE_SITE_HOST."/projeto/Assets");
 define("BASE_BD_HOST", "localhost:3306");
